c67af29f840e05191e126aebbf4b17a36870fcda
SanjayRJ28 <57896653+SanjayRJ28@users.noreply.github.com>
2020-05-31 20:23:38 +0200

    Begonen met de Python curus

---
+++ Python Intro.py
@@ -0,0 +1,9 @@
+#Informatie van de gebruiker krijgen
+name = input("Voer hier uw naam in: ")
+print(name)
+
+#Lege regels printen verbeterd het lezen van stukjes tekst.
+print("Hello world")
+print()
+print("Zag je deze lege regel?")
+print("een lege regel \nin het midden van een string")
